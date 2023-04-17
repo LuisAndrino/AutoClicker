1fcc3637b9525530389d7369fe8d667195e0b05a
Luis Andrino <67714289+LuisAndrino@users.noreply.github.com>
2023-04-17 18:50:23 +0000

    prueba uwu

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == "miComando") {
+      // Realiza la acción deseada cuando se oprime la tecla correspondiente
+    
+      let uwu = document.getElementsByClassName("button button-green-dimmed mx-auto h-9 px-3 text-2xs")
+      for(u of uwu){
+        u.click()
+        }
+      console.log("uwu")
+    }
+  });
+  
